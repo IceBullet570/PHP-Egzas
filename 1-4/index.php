f6f09ece998cf3574e1ddc19347e71a58ce35018
IceBullet570 <109753557+IceBullet570@users.noreply.github.com>
2022-11-25 18:16:41 +0200

    1 Uzduotis v1.0

--- 1-4/index.php
+++ 1-4/index.php
@@ -15,7 +15,7 @@ $numbers = [
     550,
 ];
 
-function exercise1($number)
+function exercise1($number) : int
     {
     return !($number & 1);
     }
@@ -24,4 +24,24 @@ $evenNumbers = array_filter($numbers, "exercise1");
 
 $evenNumbersSum = array_sum($evenNumbers);
 
-echo ($evenNumbersSum);
+echo ("1 Uzduotis. Lyginiu skaiciu suma = " . $evenNumbersSum);
+
+/*
+ 2. Grąžinkite visų skaičių, esančių $numbers masyve, sandaugą (1 balas), +0.5 jeigu array funkcijos naudojamos
+*/
+
+$numbers2 = [
+    [1, 3, 5],
+    [55, 87, 100],
+    [12],
+    [],
+];
+
+function exercise2($number) : int
+{
+    return array_product ($number);
+}
+
+$arrayMultiplication = ($numbers2);
+
+// echo ($arrayMultiplication);
