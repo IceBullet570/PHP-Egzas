4a2f1c08a76f57a2d143653e2f422a79359ea370
IceBullet570 <109753557+IceBullet570@users.noreply.github.com>
2022-11-26 17:09:16 +0200

    4 tasks finished

--- 1-4/index.php
+++ 1-4/index.php
@@ -106,7 +106,7 @@ function exercise3()
  4. Pakoreguokite 3 užduotį taip, kad ji duomenis rašytų ne į terminalą, o spausdintų į failą. (1 balas)
 */
 
-function exercises4()
+function exercise4()
 {
 
 }
--- 7/index.php
+++ 7/index.php
@@ -1,7 +1,28 @@
-<?php
-
 /**
 PAPILDOMAS
 7. Parašykite programą, kuri sugeneruotų ornamentą: https://iili.io/H3J974e.png .
 Ornamentas turi būti sugeneruotas naudojant HTML ir PHP. (2 balai)
  **/
+
+<html>
+<head>
+
+</head>
+<body>
+    <table>
+        <tbody>
+            <?php for ($row =0; $row < 7; $row++) { ?>
+            <tr>
+                <?php for ($column = 0; $column < 7; $column++) { 
+                    $displayColor = 'white';
+                    if ((($column == 0 or $column == 6) and ($row > 5 or $row < 1)) or $row == $column and $column > -1 and $column < 7 or ($column == 2 and $row == 4) or ($column == 4 and $row == 2) or ($row == 1 and $column == 5) or ($row == 5 and $column == 1)) {
+                        $displayColor = 'grey';
+                    } ?>
+                    <td style="width: 100px; height: 100px; background-color: <?= $displayColor; ?>; border: solid 1px #000000"></td>
+                <?php } ?>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</body>
+</html>
